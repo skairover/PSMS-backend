84c86b51d72d0d040ebd921c08b189ef5f8a1ef6
skairover <sofianehot01@gmail.com>
2026-03-09 16:05:34 +0100

    adding CORS

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://team18.vercel.app', // replace with your Vercel URL
+  credentials: true, // if you are using cookies or auth
+}));
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));   
 
