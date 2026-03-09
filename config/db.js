b8ff65b05ef81ab23f62367699efda13551d239e
skairover <sofianehot01@gmail.com>
2026-03-09 17:16:36 +0100

    something

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB connected");
-};
+  await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,
+})};
 
 module.exports = connectDB;
